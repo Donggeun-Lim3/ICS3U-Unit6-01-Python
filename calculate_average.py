bdba5f4135f696f89c68a1ce07a22b747e61cdef
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2021-01-15 03:04:13 +0000

    finds smallest number in 10 random numbers

--- calculate_average.py
+++ calculate_average.py
@@ -11,16 +11,16 @@ def main():
     # this function displays 10 random number with the array
 
     my_numbers = []
+    average = 0
 
     for loop_counter in range(0, 10):
         a_number = random.randint(1, 100)
         my_numbers.append(a_number)
+        average = average + my_numbers[loop_counter]
         print("The random number is: {0} ".format(my_numbers[loop_counter]),
               end="" + "\n")
-    average = (my_numbers[0] + my_numbers[1] + my_numbers[2] + my_numbers[3]
-               + my_numbers[4] + my_numbers[5] + my_numbers[6] + my_numbers[7]
-               + my_numbers[8] + my_numbers[9]) / 10
     print("")
+    average = average / 10
     print("The average is {0}".format(average))
 
 
